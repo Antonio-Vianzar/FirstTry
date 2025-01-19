d203b6ffbc79ac8461137056c47da3e55da64a05
Antonio-Vianzar <antoniovianzar1945@gmail.com>
2025-01-19 16:24:52 +0700

    Game over screen and play again button

--- Assets/Logic.cs
+++ Assets/Logic.cs
@@ -1,10 +1,12 @@
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Logic : MonoBehaviour
 {
     public int Score;
     public Text scoretext;
+    public GameObject gameoverscreen;
 
     [ContextMenu("Increase Score")]
     public void addscore(int scoretoadd)
--- Assets/Skrip.cs
+++ Assets/Skrip.cs
@@ -4,17 +4,25 @@ public class Skrip : MonoBehaviour
 {
     public Rigidbody2D Mybody;
     public float flapstrenght;
+    public Logic logic;
+    public bool birdalive = true;
     void Start()
     {
         gameObject.name = "Red Seed";
+        logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<Logic>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) == true)
+        if (Input.GetKeyDown(KeyCode.Space) && birdalive == true)
         {
             Mybody.linearVelocity = Vector2.up * flapstrenght;
         }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        logic.gameover();
+        birdalive = false;
     }
 }
